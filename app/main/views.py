9daa224ecbc26b56bd5cb6ceb45e208cd5cdc5f8
el1syum <kilmol606@gmail.com>
2024-07-17 19:38:31 +0300

    created to url path: (home; about)

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/index.html')
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('About page')
