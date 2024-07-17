cb7632fd334352ae27b5976dee85cb5cfff0ac28
el1syum <kilmol606@gmail.com>
2024-07-18 02:03:11 +0300

    updated category model

--- app/goods/models.py
+++ app/goods/models.py
@@ -2,8 +2,10 @@ from django.db.models import CharField, Model, SlugField
 
 
 class Categories(Model):
-    name = CharField(max_length=150, unique=True)
-    slug = SlugField(unique=True, blank=True, null=True)
+    name = CharField(max_length=150, unique=True, verbose_name='Название категории')
+    slug = SlugField(unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
