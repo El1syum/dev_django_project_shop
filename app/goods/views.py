1c5211a0e831fb3b63e2a600efe3a5f87c87d536
el1syum <kilmol606@gmail.com>
2024-07-18 07:54:29 +0300

    get product and send to templates

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
+
     context = {
-        'title': 'Каталог'
+        'title': 'Каталог',
+        'goods': goods
     }
 
     return render(request, 'goods/catalog.html', context)
