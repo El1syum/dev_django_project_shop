15884a106f3e2148a19b7d8f3c9913f20b8d004f
el1syum <kilmol606@gmail.com>
2024-07-18 07:26:24 +0300

    send categories to templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def home(request: HttpRequest) -> HttpResponse:
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home | Главная',
         'content': 'Магазин мебели HOME',
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
