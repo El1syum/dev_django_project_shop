13e5aaa4d5f78f37547ee0543b18ef8447147189
el1syum <kilmol606@gmail.com>
2024-08-23 23:10:08 +0300

    added query search

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,6 +1,18 @@
+from django.db.models import Q
+
 from goods.models import Products
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+
+    keywords = [word for word in query.split() if len(word) > 2]
+
+    q_objects = Q()
+
+    for kw in keywords:
+        q_objects |= Q(description__contains=kw)
+        q_objects |= Q(name__contains=kw)
+
+    return Products.objects.filter(q_objects)
